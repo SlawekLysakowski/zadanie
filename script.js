6441b353fd72f50eba6b2e7f3543d8026f50caee
SlawekLysakowski <slawku1@gmail.com>
2023-09-22 12:59:29 +0200

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+// Stwórz stronę składającą się z nagłówka, części głównej oraz stopki Użyj znaczników HTML5.
+// Dołącz zewnętrzne arkusze stylów oraz plik skryptowy. 
+// Strona powinna posiadać:
+// 1) Menu główne w nagłówku
+// 2) Zdjęcie w części głównej
+// 3) Listę wypunktowaną w stopce
+
+// W arkuszach stylów zamieść style, które ułożą menu główne w poziomie, wyświetlą zdjęcie
+// oraz listę wypunktowaną. 
+// W skrypcie umieść funkcję, która będzie na kliknięcie w jeden z przycisków menu, podmieniać kolor tła strony.
+// Całość prześli do repozytorium na github.com.
+
+// Adres do repozytorium prześlij jako rozwiązanie.
+
+
+document.querySelector('.nav-item-1')
+
+const changeColor = function(color) {
+    document.querySelector( '.container').style.backgroundColor=color
+};
